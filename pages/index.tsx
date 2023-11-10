988336519fc856560aefa6e97738dbd83fd8311a
Huig van der Waal <huigvanderwaal@gmail.com>
2023-11-10 18:02:04 +0100

    images

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,54 +68,48 @@ const Home: NextPage = () => {
 
   const renderPropertyLinks = () => {
     return propertiesData.map((property) => (
-      <div key={property.number} style={{ backgroundColor: property.colorCode }} className="relative flex flex-col space-y-3 bg-whiteX shadow-2xl bg-yellow-500X">
-        {/* <div className="absolute top-3 right-3 font-bold">{property.number}</div> */}
-        <div className="flex flex-col space-y-3 p-3 pt-8">
-          <img src={`/${property.image}`} alt={property.name} className="w-20 h-20" />
-          <div className="relative">
-          {!hasVisitedProperty(property.name) ? (
-          <>
-            <h2 className="relative font-spaghetiX text-whiteX z-10 text-2xl">{property.description}</h2>
-          </>
-        ) : (
-          <>
-            <p>Je bent {property.name}</p>
-            <h2 className="text-4xl">{nftData[property.name.toLowerCase()] || 'Not available'}</h2>
-          </>
-        )}
+      <div key={property.number} style={{ backgroundColor: property.colorCode }} className="relative flex flex-col bg-white shadow-2xl">
+        <div className="relative flex flex-col h-80">
+          {(passwords[property.name] === property.password || hasVisitedProperty(property.name)) && (
+            <img className="absolute inset-0 z-20 w-full h-full object-cover" src={`/${property.image}`} alt={property.name} />
+          )}
+          <img className="absolute inset-0 w-full h-full object-cover opacity-50" src="/logo.svg" />
         </div>
-      </div>
         
-        <div className="flex flex-col space-y-px p-3 bg-white">
-        {!hasVisitedProperty(property.name) ? (
-          <>
-             <input
-              className="border border-black/30 p-3 w-full text-center rounded-none shadow-sm"
-              type="password"
-              placeholder="Wachtwoord"
-              onChange={(e) => handlePasswordChange(property.name, e.target.value)}
-            />
-            {passwords[property.name] === property.password && (
+        <div className="flex flex-col space-y-3 p-3 bg-white">
+          {!hasVisitedProperty(property.name) ? (
+            <>
+              <h2 className="text-4xl">{property.description} </h2>
+              <input
+                className="border border-black/30 p-3 w-full text-center rounded-none shadow-sm"
+                type="password"
+                placeholder="Wachtwoord"
+                onChange={(e) => handlePasswordChange(property.name, e.target.value)}
+              />
+              {passwords[property.name] === property.password && (
+                <Link href={`/kaart/${property.name.toLowerCase()}`}>
+                  <div className="w-full mt-2 inline-block bg-black text-center text-white p-3">Volgende!</div>
+                </Link>
+              )}
+            </>
+          ) : (
+            <>
+              <div className="">
+                <p>Je bent {property.name}</p>
+                <h2 className="text-4xl">{nftData[property.name.toLowerCase()] || ''}</h2>
+              </div>
               <Link href={`/kaart/${property.name.toLowerCase()}`}>
-                <div className="w-full mt-2 inline-block bg-black text-center text-white p-3">Volgende!</div>
+                <div className="w-full mt-2 inline-block bg-black text-center text-white p-3">Bekijk</div>
               </Link>
-            )}
-          </>
-        ) : (
-          <>
-            <Link href={`/kaart/${property.name.toLowerCase()}`}>
-              <div className="w-full mt-2 inline-block bg-black text-center text-white p-3">Bekijk</div>
-            </Link>
-          </>
-        )}
-      </div>
-
+            </>
+          )}
+        </div>
       </div>
     ));
   };
 
   return (
-    <div className="max-w-sm mx-auto font-spaghetiX">
+    <div className="max-w-sm mx-auto">
       {address ? (
         <div className="p-5 pt-16 bg-yellow-500">
           <div className="fixed inset-0 bottom-auto z-20 py-2 text-center bg-yellow-500 shadow-xl">
